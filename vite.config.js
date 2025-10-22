2daa5e3ba4820ad79ed38052a372730a128d6621
dylanc0214 <dylanchow0214@gmail.com>
2025-10-22 23:40:31 +0800

    fix: import correct tailwindcss plugin in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss' // Import tailwind
-import autoprefixer from 'autoprefixer' // Import autoprefixer
+import tailwindcss from '@tailwindcss/postcss' // <-- THIS IS THE FIX. Use @tailwindcss/postcss
+import autoprefixer from 'autoprefixer' // <-- Import Autoprefixer
 
 // https://vite.dev/config/
 export default defineConfig({
